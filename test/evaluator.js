ffb0c5a9bd8d55ebad5516ada7e03aa1f6b5e75b
Curtis Steckel <steckel@squareup.com>
2014-11-18 13:38:12 -0800

    evaluator subtraction, multiplication and division

--- test/evaluator.js
+++ test/evaluator.js
@@ -7,3 +7,24 @@ assert.equal(evaluator([
   {type: "NUMBER", value: "1"},
   {type: "OPERATOR", value: "+", precedence:1}
 ]), 2);
+
+// 2 1 - => 1
+assert.equal(evaluator([
+  {type: "NUMBER", value: "2"},
+  {type: "NUMBER", value: "1"},
+  {type: "OPERATOR", value: "-", precedence:1}
+]), 1);
+
+// 5 2 * => 10
+assert.equal(evaluator([
+  {type: "NUMBER", value: "5"},
+  {type: "NUMBER", value: "2"},
+  {type: "OPERATOR", value: "*", precedence:2}
+]), 10);
+
+// 10 2 / => 5
+assert.equal(evaluator([
+  {type: "NUMBER", value: "10"},
+  {type: "NUMBER", value: "2"},
+  {type: "OPERATOR", value: "/", precedence:2}
+]), 5);
